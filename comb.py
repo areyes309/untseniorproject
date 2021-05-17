234106fbba990ed778588f635bd5e159278a4795
Angelica Reyes <16010109+areyes309@users.noreply.github.com>
2021-05-16 19:01:25 -0500

    Add files via upload

---
+++ comb.py
@@ -0,0 +1,14 @@
+import glob
+import os
+import pandas as pd
+
+
+os.chdir("/home/fadi/Desktop/bagpictures/")
+
+extension = 'csv'
+
+all_filenames = [i for i in glob.glob('.{}'.format(extension))]
+
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+
+combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
